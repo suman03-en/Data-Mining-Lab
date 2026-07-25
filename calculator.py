2ab6e01a181faae22a9609344d052221cdc1882c
suman phuyal <sfuyal59@gmail.com>
2026-07-25 12:41:19 +0545

    Complete lab1

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(a: int, b: int):
+    return a + b
+
+def subtract(a: int, b: int):
+    return a - b
+
+def multiply(a: int, b: int):
+    return a * b
+
+def divide(a: int, b: int):
+    return a / b
